f886f7bbd9381f2d20372e9c0016ef3b99f87ebd
dk98 <dk98@dk98s-MacBook-Pro.local>
2024-10-26 22:10:44 +0700

    tạo 1 component dùng chung

---
+++ lib/gradient_container.dart
@@ -0,0 +1,21 @@
+import 'package:first_app/styled_text.dart';
+import 'package:flutter/material.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+            colors: [Colors.black, Colors.white, Colors.blue],
+            begin: startAlignment,
+            end: endAlignment),
+      ),
+      child: const Center(child: StyledText('Hello, World!')),
+    );
+  }
+}
